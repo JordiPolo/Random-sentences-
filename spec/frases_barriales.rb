926e5fc8641f47d6ed8a3ab17310872a7428c184
Jordi Polo <mumismo@gmail.com>
2011-03-04 09:37:44 +0100

    new specs, link use url helper, link mimics FB colour

--- spec/frases_barriales.rb
+++ spec/frases_barriales.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Sentences application" do
+describe "My sentences" do
   
   include Rack::Test::Methods
 
@@ -8,22 +8,45 @@ describe "Sentences application" do
     @app ||= Sinatra::Application
   end
 
-  context "/" do
-    it "should get some response" do
-      get '/'
-      last_response.should be_ok
+  ["/", "/todas_las_frases", "/mumimama"].each do |page|
+    context "for the url #{page}" do
+      it "should get some response" do
+        get page
+        last_response.should be_ok
+      end
+      
+      it "should return the correct content-type" do
+        get page
+        last_response.headers["Content-Type"].should == "text/html;charset=utf-8"
+      end
+      
+      it "should return some content" do
+        get page
+        last_response.headers["Content-Length"].to_i.should > 0
+      end
     end
     
-    it "should return the correct content-type" do
-      get '/'
-      last_response.headers["Content-Type"].should == "text/html;charset=utf-8"
-    end
   end
   
   it "should return 404 when page cannot be found" do
     get '/404'
     last_response.status.should == 404
   end
+  
+    
+  it "should be css in the headers when getting css" do
+    pending
+    get '/styles.css'
+    last_response.headers["Content-Type"].should == 'text/css;charset=utf-8'
+  end
+  
+  it "should respond to /style.css" do
+    pending
+    get '/styles.css'
+    last_response.should be_ok
+  end
+  
+  
   
 end
 
