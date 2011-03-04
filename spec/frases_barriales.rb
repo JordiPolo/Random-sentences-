27d850085772ea07d1dacd51cbdba6c8764121be
Jordi Polo <mumismo@gmail.com>
2011-03-04 02:45:25 +0100

    redirect to server name

--- spec/frases_barriales.rb
+++ spec/frases_barriales.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "My App" do
+describe "Sentences application" do
   
   include Rack::Test::Methods
 
@@ -8,20 +8,22 @@ describe "My App" do
     @app ||= Sinatra::Application
   end
 
-  
-  it "should respond to /" do
-    get '/'
-    last_response.should be_ok
+  context "/" do
+    it "should get some response" do
+      get '/'
+      last_response.should be_ok
+    end
+    
+    it "should return the correct content-type" do
+      get '/'
+      last_response.headers["Content-Type"].should == "text/html;charset=utf-8"
+    end
   end
   
-  it "should return the correct content-type when viewing root" do
-    get '/'
-    last_response.headers["Content-Type"].should == "text/html;charset=utf-8"
-  end
-
   it "should return 404 when page cannot be found" do
     get '/404'
     last_response.status.should == 404
   end
+  
 end
 
